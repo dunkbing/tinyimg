964b0c029f9fe89b96225859109e266061056341
Bùi Đặng Bình <dangbinh4869@gmail.com>
2024-02-29 00:03:31 +0700

    Update Promote.tsx

--- components/Promote.tsx
+++ components/Promote.tsx
@@ -1,14 +1,14 @@
 export function Promote() {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md my-4">
+    <div className="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md my-4 px-2">
       <img
         src="/text2audio.png"
-        alt="Ad Image"
+        alt="Text2Audio"
         className="h-24 object-cover"
       />
       <div className="px-4 pb-4">
         <h2 className="text-xl font-semibold text-gray-800 mb-1">
-          Also check out text2audio.cc!
+          Also, check out text2audio.cc!
         </h2>
         <p className="text-gray-600 mb-2">
           Convert text to audio easily with text2audio.cc. Explore a new way to
